9c0040c4a39ea79c45fe086677a0ea8a6ffe016b
Steffen Ehrmann <steffen.ehrmann@idiv.de>
2019-02-26 17:16:20 +0100

    allow more different objects to set a window from

--- R/setWindow.R
+++ R/setWindow.R
@@ -1,7 +1,8 @@
 #' Set the reference window of a spatial object.
 #' @param x the object in which to set the reference window.
-#' @param to [\code{data.frame(1)}]\cr two oposing corners or all four
-#'   corners of the rectangle.
+#' @param to [\code{data.frame(1)}]\cr two oposing corners.
+#' @example
+#'
 #' @name setWindow
 #' @rdname setWindow
 NULL
@@ -17,20 +18,30 @@ if(!isGeneric("setWindow")){
 }
 
 #' @rdname setWindow
+#' @importFrom checkmate testSubset assert
 #' @importFrom tibble tibble
 #' @export
 setMethod(f = "setWindow",
           signature = "geom",
           definition = function(x, to){
-            stopifnot(all(c("x", "y") %in% colnames(to)))
-            if(nrow(to) == 4){
-              x@window <- to[c("x", "y")]
-            } else if(nrow(to) == 2){
-              x@window <- tibble(x = rep(to$x, each = 2),
-                                 y = c(to$y, rev(to$y)))
+            if("Extent" %in% class(to)){
+              xVals <- c(to@xmin, to@xmax)
+              yVals <- c(to@ymin, to@ymax)
+            } else if(is.data.frame(to)){
+              assertDataFrame(x = to, nrows = 2, min.cols = 2)
+              assertNames(names(to), must.include = c("x", "y"))
+              xVals <- c(to$x[1], to$x[2])
+              yVals <- c(to$y[1], to$y[2])
+            } else if(is.vector(to)){
+              names(to) <- tolower(names(to))
+              assertNames(names(to), must.include = c("xmin", "xmax", "ymin", "ymax"))
+              xVals <- c(to["xmin"], to["xmax"])
+              yVals <- c(to["ymin"], to["ymax"])
             } else{
               stop("no suitable window provided.")
             }
+            x@window <- tibble(x = rep(xVals, each = 2),
+                               y = c(yVals, rev(yVals)))
             return(x)
           }
 )
