031c5778924f295dcbd06bb001b56b9dc8536f7e
Steffen Ehrmann <steffen.science@funroll-loops.de>
2019-03-08 00:26:59 +0100

    setTable for sp

--- R/setTable.R
+++ R/setTable.R
@@ -21,7 +21,7 @@ if(!isGeneric("setTable")){
 setMethod(f = "setTable",
           signature = "geom",
           definition = function(x, table){
-            stopifnot(is.data.frame(table))
+            assertDataFrame(table)
             stopifnot(any(names(table) %in% "fid"))
             nIDs <- length(x@attr$fid)
             x@attr <- left_join(x@attr, table)
@@ -30,11 +30,39 @@ setMethod(f = "setTable",
 )
 
 #' @rdname setTable
+#' @importFrom dplyr left_join bind_cols
+#' @importFrom checkmate assertDataFrame assertTRUE
+#' @importFrom sp SpatialPointsDataFrame SpatialPixelsDataFrame
+#'   SpatialMultiPointsDataFrame SpatialLinesDataFrame SpatialPolygonsDataFrame
 #' @export
 setMethod(f = "setTable",
           signature = "Spatial",
           definition = function(x, table){
+            assertDataFrame(table)
+            assertTRUE(length(x) == dim(table)[1])
 
+            if(grepl("DataFrame", class(x))){
+              if(any(colnames(table) %in% colnames(x@data))){
+                x@data <- left_join(x@data, table)
+              } else{
+                x@data <- bind_cols(x@data, table)
+              }
+              out <- x
+            } else{
+              if(class(x) == "SpatialPoints"){
+                out <- SpatialPointsDataFrame(coords = x, data = table)
+              } else if(class(x) == "SpatialPixels"){
+                out <- SpatialPixelsDataFrame(points = x, data = table)
+              } else if(class(x) == "SpatialMultiPoints"){
+                out <- SpatialMultiPointsDataFrame(coords = x, data = table)
+              } else if(class(x) == "SpatialLines"){
+                out <- SpatialLinesDataFrame(sl = x, data = table, match.ID = FALSE)
+              } else if(class(x) == "SpatialPolygons"){
+                out <- SpatialPolygonsDataFrame(Sr = x, data = table)
+              }
+            }
+
+            return(x)
           }
 )
 
