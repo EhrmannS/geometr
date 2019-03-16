758a97d2dab0ad893275899f3ef5c71a9db9ca09
Steffen Ehrmann <steffen.science@funroll-loops.de>
2019-03-17 00:17:58 +0100

    modernize function, enable lines

--- R/gt_as_sp.R
+++ R/gt_as_sp.R
@@ -3,50 +3,23 @@
 #' An object of class \code{Spatial*} is the sp-package representation of a
 #' \code{geom}.
 #' @param geom [\code{geom}]\cr Object of class \code{\link{geom}}.
-#' @param crs [\code{character(1)}]\cr corrdinate reference system of the
-#'   object in proj4 notation.
 #' @return Depending on the provided geometry either a
 #'   \code{\link{SpatialPointsDataFrame}}, \code{\link{SpatialLinesDataFrame}}
 #'   or \code{\link{SpatialPolygonsDataFrame}} object.
 #' @examples
-#' library(magrittr)
-#'
-#' somePoints <- data.frame(X = c(5027609, 5190599, 5326537, 5222810,
-#'                          5234735, 5281527, 5189955, 5041066),
-#'                          Y = c(3977612, 3971119, 4028167, 3997230,
-#'                          4060164, 4117856, 4118207, 4062838),
-#'                          fid = c(1:8))
-#'
-#' pointsGeom <- gs_point(anchor = somePoints)
-#' polyGeom <- gt_group(geom = pointsGeom, index = c(rep(1, 8))) %>%
-#'   gs_polygon()
-#'
-#' (spPoints <- gt_as_sp(geom = pointsGeom,
-#'   crs = "+proj=laea +lat_0=52 +lon_0=10 +x_0=4321000 +y_0=3210000 +ellps=GRS80 +units=m +no_defs"))
-#' (spPolygon <- gt_as_sp(geom = polyGeom,
-#'   crs = "+proj=laea +lat_0=52 +lon_0=10 +x_0=4321000 +y_0=3210000 +ellps=GRS80 +units=m +no_defs"))
+#' (spPoints <- gt_as_sp(geom = gtGeoms$point))
+#' (spLines <- gt_as_sp(geom = gtGeoms$line))
+#' (spPolygon <- gt_as_sp(geom = gtGeoms$polygon))
 #' @importFrom checkmate assertClass assertCharacter
 #' @importFrom raster crs
-#' @importFrom sp SpatialPoints SpatialPointsDataFrame Polygon Polygons
-#'   SpatialPolygons SpatialPolygonsDataFrame
+#' @importFrom sp SpatialPoints SpatialPointsDataFrame Line Lines SpatialLines
+#'   SpatialLinesDataFrame Polygon Polygons SpatialPolygons
+#'   SpatialPolygonsDataFrame
 #' @export
 
-gt_as_sp <- function(geom, crs = NULL){
+gt_as_sp <- function(geom = NULL){
 
   assertClass(geom, classes = "geom")
-  assertCharacter(crs, fixed = "+proj", null.ok = TRUE)
-  if(is.null(crs) & is.na(geom@crs)){
-    targetCRS <- as.character(NA)
-  } else if(!is.null(crs)){
-    targetCRS <- crs
-  } else if(!is.na(geom@crs)){
-    targetCRS <- geom@crs
-  }
-  if(is.na(geom@crs)){
-    sourceCrs <- as.character(NA)
-  } else{
-    sourceCrs <- geom@crs
-  }
 
   featureType <- geom@type
   vert <- geom@vert
@@ -55,52 +28,60 @@ gt_as_sp <- function(geom, crs = NULL){
   if(featureType %in% c("point")){
 
     temp <- vert[c("x", "y")]
-    geomSp <- SpatialPoints(temp, proj4string = crs(sourceCrs))
-    if(!all(names(geom@attr) %in% c("fid", "n"))){
-      attr <- geom@attr[,!names(geom@attr) %in% c("fid", "n")]
+    geomSp <- SpatialPoints(temp)
+    # if there are columns in the geoms attribute table, make a Spatial*DataFrame
+    if(!all(names(geom@attr) %in% c("fid", "gid"))){
+      attr <- geom@attr[,!names(geom@attr) %in% c("fid", "gid")]
       geomSp <- SpatialPointsDataFrame(geomSp, data = attr, match.ID = FALSE)
     }
 
-    # } else if(featureType %in% c("line")){
-    #
-    #   temp <- list()
-    #   # go through distinct ids and check whether the last coordinate is equal to the first.
-    #   for(i in seq_along(id)){
-    #     thePoly <- vert[c(1, 2)][vert$id == id[i],]
-    #     if(!all(thePoly[1,] == thePoly[dim(thePoly)[1],])){
-    #       thePoly <- rbind(thePoly, thePoly[1,])
-    #     }
-    #
-    #     # put togehter the 'Polygons' list
-    #     temp <- c(temp, Polygons(list(Polygon(thePoly)), id[i]))
-    #   }
-    #
-    #   geomSp <- SpatialLines(temp, proj4string = CRS(theCRS))
-    #   geomSp <- SpatialLinesDataFrame(geomSp, data = data.frame(id = seq_along(geomSp)), match.ID = FALSE)
+  } else if(featureType %in% c("line")){
+
+    outLines <- list()
+    outLine <- list()
+    for(i in seq_along(features)){
+      theLine <- vert[c("x", "y")][vert$fid == features[i],]
+      outLine <- c(outLine, Line(theLine))
+      outLines <- c(outLines, Lines(outLine, features[i]))
+    }
+    geomSp <- SpatialLines(outLines)
+
+    # if there are columns in the geoms attribute table, make a Spatial*DataFrame
+    if(!all(names(geom@attr) %in% c("fid", "gid"))){
+      attr <- geom@attr[,!names(geom@attr) %in% c("fid", "gid")]
+      geomSp <- SpatialLinesDataFrame(geomSp, data = attr, match.ID = FALSE)
+    }
 
   } else if(featureType %in% c("polygon")){
 
-    temp <- list()
+    outPolygons <- list()
     # go through distinct ids and check whether the last coordinate is equal to the first.
     for(i in seq_along(features)){
       thePoly <- vert[c("x", "y")][vert$fid == features[i],]
-      if(!all(thePoly[1,] == thePoly[dim(thePoly)[1],])){
-        thePoly <- rbind(thePoly, thePoly[1,])
+      outPolygon <- list()
+
+      dups <- as.numeric(duplicated(thePoly[c("x", "y")]))
+      dups <- c(0, dups[-length(dups)])
+      thePoly$subID <- 1 + cumsum(dups)
+
+      for(j in seq_along(unique(thePoly$subID))){
+        temp <- thePoly[c("x", "y")][thePoly$subID == j,]
+
+        outPolygon <- c(outPolygon, Polygon(temp))
       }
 
       # put togehter the 'Polygons' list
-      temp <- c(temp, Polygons(list(Polygon(thePoly)), features[i]))
+      outPolygons <- c(outPolygons, Polygons(outPolygon, features[i]))
     }
 
     # make a SpatialPolygon out of that
-    geomSp <- SpatialPolygons(temp, proj4string = crs(sourceCrs))
-    if(!all(names(geom@attr) %in% c("fid", "n"))){
-      attr <- geom@attr[,!names(geom@attr) %in% c("fid", "n")]
+    geomSp <- SpatialPolygons(outPolygons)
+    if(!all(names(geom@attr) %in% c("fid", "gid"))){
+      attr <- geom@attr[,!names(geom@attr) %in% c("fid", "gid")]
       geomSp <- SpatialPolygonsDataFrame(geomSp, data = attr, match.ID = FALSE)
     }
 
   }
-  geomSp <- setCRS(x = geomSp, crs = targetCRS)
 
   return(geomSp)
 }
