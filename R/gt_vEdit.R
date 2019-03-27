ea9c5231a9226090ac68c9bffac075a36f623d4c
Steffen Ehrmann <steffen.science@funroll-loops.de>
2019-03-27 08:53:59 +0100

    start new function

---
+++ R/gt_vEdit.R
@@ -0,0 +1,11 @@
+#' Edit point geometries
+#'
+#' \code{gt_vEdit} allows to edit the vertices of a \code{geom} of type
+#' \code{point}.
+#' @param geom [\code{geom}]\cr object of class \code{\link{geom}} and type
+#'   \code{point}.
+#'
+
+gt_vEdit <- function(geom){
+
+}
