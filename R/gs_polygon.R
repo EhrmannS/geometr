edc2ea955f757aefecc64275c5548fb3871011da
Steffen Ehrmann <steffen.science@funroll-loops.de>
2019-07-29 18:12:07 +0300

    replace sketching procedure with new functions gt_sketch

--- R/gs_polygon.R
+++ R/gs_polygon.R
@@ -1,19 +1,19 @@
 #' Create a polygon geometry
 #'
 #' Create any (regular) polygon geometry (of class \code{\link{geom}}) either by
-#' specifying its parameters or by sketching it.
+#' specifying anchor values or by sketching it.
 #' @param anchor [\code{data.frame(1)}]\cr Object to derive the \code{geom}
 #'   from. It must include column names \code{x}, \code{y} and optinal variables
 #'   such as \code{fid}; see Examples.
 #' @param window [\code{data.frame(1)}]\cr in case the reference window deviates
 #'   from the bounding box of \code{anchor} (minimum and maximum values),
 #'   specify this here.
-#' @param template [\code{RasterLayer(1)} | \code{matrix(1)}]\cr Gridded object
-#'   that serves as template to sketch the geometry.
 #' @param features [\code{integerish(1)}]\cr number of geometries to create.
 #' @param vertices [\code{integerish(.)}]\cr number of vertices per geometry;
 #'   will be recycled if it does not have as many elements as specified in
 #'   \code{features}.
+#' @param sketch [\code{RasterLayer(1)}|\code{matrix(1)}]\cr Gridded object that
+#'   serves as template to sketch the geometry.
 #' @param regular [\code{logical(1)}]\cr should the polygon be regular, i.e.
 #'   point symmetric (\code{TRUE}) or should the vertices be selected according
 #'   to \code{anchor} or \code{vertices} (\code{FALSE}, default)?
@@ -21,13 +21,13 @@
 #'   (\code{TRUE}, default), or should it be aligned according to the second
 #'   click (\code{FALSE}); only relevant if \code{regular = TRUE}.
 #' @param ... [various]\cr additional arguments; see Details.
-#' @details The arguments \code{anchor} and \code{template} indicate how the
+#' @details The arguments \code{anchor} and \code{sketch} indicate how the
 #'   geometry is created: \itemize{ \item \code{anchor}: if set, the geometry is
 #'   created parametrically, the input provided is used to parameterise the
 #'   geometry \itemize{ \item if \code{regular = FALSE} the resulting geometry
 #'   is the boundary per feature, \item if \code{regular = TRUE}, only the first
 #'   two vertices are considered, as center and indicating the (outer) radius.}
-#'   \item \code{template}: if set, the geometry is created interactively, by
+#'   \item \code{sketch}: if set, the geometry is created interactively, by
 #'   clicking into the plot.}
 #'
 #'   Possible additional arguments are: \itemize{ \item verbose = TRUE/FALSE
@@ -67,16 +67,19 @@
 #' @importFrom rlang !!
 #' @export
 
-gs_polygon <- function(anchor = NULL, window = NULL, template = NULL, features = 1,
-                       vertices = NULL, regular = FALSE, fixed = TRUE, ...){
+gs_polygon <- function(anchor = NULL, window = NULL, features = 1, vertices = NULL,
+                       sketch = NULL, regular = FALSE, fixed = TRUE, ...){
 
   # check arguments
   anchor <- .testAnchor(x = anchor, ...)
   theWindow <- .testWindow(x = window, ...)
-  template <- .testTemplate(x = template, ...)
+  assertIntegerish(x = features, len = 1, lower = 1)
+  assertIntegerish(x = vertices, min.len = 1, lower = 2, any.missing = FALSE, null.ok = TRUE)
+  assertLogical(x = regular)
+  assertLogical(x = fixed)
 
-  if(is.null(anchor) & is.null(template)){
-    stop("please provide either 'anchor' or 'template'.")
+  if(is.null(anchor) & is.null(sketch)){
+    stop("please provide anchor values.")
   }
   if(!is.null(anchor)){
     if(anchor$type == "geom"){
@@ -94,142 +97,104 @@ gs_polygon <- function(anchor = NULL, window = NULL, template = NULL, features =
       }
     }
   }
-  assertIntegerish(x = features, len = 1, lower = 1)
-  assertLogical(x = regular)
-  assertLogical(x = fixed)
-  if(is.null(anchor)){
-    assertIntegerish(vertices, min.len = 1, lower = 2, any.missing = FALSE)
-    if(length(vertices) != features){
-      vertices <- rep(vertices, length.out = features)
-    }
+
+  # if anchor does not exists, make it
+  if(!is.null(sketch)){
+
+    template <- .testTemplate(x = sketch, ...)
+    theGeom <- gt_sketch(template = template$obj, shape = "polygon", features = features, vertices = vertices, regular = regular, fixed = fixed)
+
   } else{
-    assertIntegerish(vertices, min.len = 1, lower = 2, any.missing = FALSE, null.ok = TRUE)
-  }
 
-  # get some raster properties
-  if(!is.null(template)){
-    if(template$type == "RasterLayer"){
-      tempName <- names(template$obj)
-      dims <- dim(template$obj)
-      projection <- getCRS(x = template$obj)
-    } else{
-      tempName <- "layer"
-      dims <- dim(template$obj)
-      projection <- NA
-    }
-  } else{
-    tempName <- "layer"
-    projection <- NA
-  }
+    theVertices <- theFeatures <- theGroups <- NULL
+    for(i in 1:features){
 
-  theVertices <- theFeatures <- theGroups <- NULL
-  for(i in 1:features){
+      if(anchor$type == "geom"){
+        # }
 
-    # if anchor does not exists, make it
-    if(is.null(anchor)){
+        # } else if(anchor$type == "geom"){
 
+        if(is.null(theWindow)){
+          theWindow <- anchor$obj@window
+        }
+        tempAnchor <- anchor$obj@vert[anchor$obj@vert$fid == i,]
+        openingAngle <- atan((tempAnchor$x[1] - tempAnchor$x[2]) / (tempAnchor$y[1] - tempAnchor$y[2])) * 180 / pi
+        tempFeatures <- anchor$obj@feat[anchor$obj@feat$fid == i,]
+        tempGroups <- anchor$obj@group[anchor$obj@group$gid == i,]
+        projection <- getCRS(x = anchor$obj)
+
+      } else if(anchor$type == "df"){
+
+        if(is.null(theWindow)){
+          theWindow = tibble(x = c(min(anchor$obj$x), max(anchor$obj$x), max(anchor$obj$x), min(anchor$obj$x), min(anchor$obj$x)),
+                             y = c(min(anchor$obj$y), min(anchor$obj$y), max(anchor$obj$y), max(anchor$obj$y), min(anchor$obj$y)))
+        }
+        if("fid" %in% names(anchor$obj)){
+          tempAnchor <- anchor$obj[anchor$obj$fid == i, ]
+        } else {
+          tempAnchor <- anchor$obj
+          tempAnchor <- bind_cols(tempAnchor, fid = rep(i, length.out = length(anchor$obj$x)))
+        }
+        openingAngle <- atan((tempAnchor$x[1] - tempAnchor$x[2]) / (tempAnchor$y[1] - tempAnchor$y[2])) * 180 / pi
+        tempFeatures <- tibble(fid = i, gid = i)
+        tempGroups <- tibble(gid = i)
+        projection <- NA
+
+      }
+
+      # build a regular geometry
       if(regular){
-        message("please click the polygons' center and the first vertex.")
-        clicks <- 2
+        # trigonometry
+        angle <- 360/vertices[i]
+        angles <- seq(from = 90, to = 360-angle+90, by = angle) - openingAngle
+
+        radius <- dist(tempAnchor[c(1:2),])
+        cx <- tempAnchor$x[1] + radius*cos(.rad(angles))
+        cy <- tempAnchor$y[1] + radius*sin(.rad(angles))
+        theNodes <- tibble(x = cx, y = cy, fid = i)
+        theWindow <- .updateWindow(geom = theNodes, window = theWindow)
       } else{
-        message(paste0("please click the ", vertices[i], " vertices."))
-        clicks <- vertices[i]
+        theNodes <- tempAnchor[c("x", "y", "fid")]
       }
-      if(i == 1){
-        visualise(raster = template$obj)
-      }
-      theClicks <- gt_locate(samples = clicks, panel = tempName, silent = TRUE, ...)
-      theWindow <- tibble(x = c(0, dims[2], dims[2], 0, 0),
-                          y = c(0, 0, dims[1], dims[1], 0))
-      tempAnchor <- tibble(x = theClicks$x,
-                           y = theClicks$y,
-                           fid = i)
 
-      if(fixed){
-        openingAngle <- 0
-      } else{
-        # get the angle between the first and second click
-        openingAngle <- atan((theClicks$x[1] - theClicks$x[2]) / (theClicks$y[1] - theClicks$y[2])) * 180 / pi
+      # create vertices that would close rings and thus make valid polygons, check
+      # whether the first vertex has a duplicate...
+      dupBwd <- duplicated(theNodes, fromLast = TRUE)
+      if(!dupBwd[1]){
+        # ... if not, test whether there is any duplicate, hence any closed ring
+        if(any(dupBwd)){
+          theNodes <- add_row(theNodes, x = theNodes$x[1], y = theNodes$y[1], fid = theNodes$fid[1], .before = which(dupBwd))
+        } else {
+          theNodes <- add_row(theNodes, x = theNodes$x[1], y = theNodes$y[1], fid = theNodes$fid[1])
+        }
       }
-      tempFeatures <- tibble(fid = i, gid = i)
-      tempGroups <- tibble(gid = i)
 
-    } else if(anchor$type == "geom"){
-      if(is.null(theWindow)){
-        theWindow <- anchor$obj@window
+      # check whether the last vertex has a duplicate...
+      dupFwd <- duplicated(theNodes)
+      if(!dupFwd[dim(theNodes)[1]]){
+        # ... if not, test whether there is any duplicate, hence any closed ring
+        if(any(dupFwd)){
+          theNodes <- add_row(theNodes, x = theNodes$x[dim(theNodes)[1]], y = theNodes$y[dim(theNodes)[1]], fid = theNodes$fid[dim(theNodes)[1]], .after = which(dupFwd)+1)
+        } else {
+          theNodes <- add_row(theNodes, x = theNodes$x[1], y = theNodes$y[1], fid = theNodes$fid[1])
+        }
       }
-      tempAnchor <- anchor$obj@vert[anchor$obj@vert$fid == i,]
-      openingAngle <- atan((tempAnchor$x[1] - tempAnchor$x[2]) / (tempAnchor$y[1] - tempAnchor$y[2])) * 180 / pi
-      tempFeatures <- anchor$obj@feat[anchor$obj@feat$fid == i,]
-      tempGroups <- anchor$obj@group[anchor$obj@group$gid == i,]
-    } else if(anchor$type == "df"){
-      if(is.null(theWindow)){
-        theWindow = tibble(x = c(min(anchor$obj$x), max(anchor$obj$x), max(anchor$obj$x), min(anchor$obj$x), min(anchor$obj$x)),
-                           y = c(min(anchor$obj$y), min(anchor$obj$y), max(anchor$obj$y), max(anchor$obj$y), min(anchor$obj$y)))
-      }
-      if("fid" %in% names(anchor$obj)){
-        tempAnchor <- anchor$obj[anchor$obj$fid == i, ]
-      } else {
-        tempAnchor <- anchor$obj
-        tempAnchor <- bind_cols(tempAnchor, fid = rep(i, length.out = length(anchor$obj$x)))
-      }
-      openingAngle <- atan((tempAnchor$x[1] - tempAnchor$x[2]) / (tempAnchor$y[1] - tempAnchor$y[2])) * 180 / pi
-      tempFeatures <- tibble(fid = i, gid = i)
-      tempGroups <- tibble(gid = i)
+
+      theVertices <- bind_rows(theVertices, theNodes)
+      theFeatures <- bind_rows(theFeatures, tempFeatures)
+      theGroups <- bind_rows(theGroups, tempGroups)
     }
 
-    # build a regular geometry
-    if(regular){
-      # trigonometry
-      angle <- 360/vertices[i]
-      angles <- seq(from = 90, to = 360-angle+90, by = angle) - openingAngle
-
-      radius <- dist(tempAnchor[c(1:2),])
-      cx <- tempAnchor$x[1] + radius*cos(.rad(angles))
-      cy <- tempAnchor$y[1] + radius*sin(.rad(angles))
-      theNodes <- tibble(x = cx, y = cy, fid = i)
-      theWindow <- .updateWindow(geom = theNodes, window = theWindow)
-    } else{
-      theNodes <- tempAnchor[c("x", "y", "fid")]
-    }
-
-    # create vertices that would close rings and thus make valid polygons, check
-    # whether the first vertex has a duplicate...
-    dupBwd <- duplicated(theNodes, fromLast = TRUE)
-    if(!dupBwd[1]){
-      # ... if not, test whether there is any duplicate, hence any closed ring
-      if(any(dupBwd)){
-        theNodes <- add_row(theNodes, x = theNodes$x[1], y = theNodes$y[1], fid = theNodes$fid[1], .before = which(dupBwd))
-      } else {
-        theNodes <- add_row(theNodes, x = theNodes$x[1], y = theNodes$y[1], fid = theNodes$fid[1])
-      }
-    }
-
-    # check whether the last vertex has a duplicate...
-    dupFwd <- duplicated(theNodes)
-    if(!dupFwd[dim(theNodes)[1]]){
-      # ... if not, test whether there is any duplicate, hence any closed ring
-      if(any(dupFwd)){
-        theNodes <- add_row(theNodes, x = theNodes$x[dim(theNodes)[1]], y = theNodes$y[dim(theNodes)[1]], fid = theNodes$fid[dim(theNodes)[1]], .after = which(dupFwd)+1)
-      } else {
-        theNodes <- add_row(theNodes, x = theNodes$x[1], y = theNodes$y[1], fid = theNodes$fid[1])
-      }
-    }
-
-    theVertices <- bind_rows(theVertices, theNodes)
-    theFeatures <- bind_rows(theFeatures, tempFeatures)
-    theGroups <- bind_rows(theGroups, tempGroups)
+    theGeom <- new(Class = "geom",
+                   type = "polygon",
+                   vert = theVertices,
+                   feat = theFeatures,
+                   group = theGroups,
+                   window = theWindow,
+                   scale = "absolute",
+                   crs = as.character(projection),
+                   history = list(paste0("geometry was created as 'polygon'.")))
   }
-
-  theGeom <- new(Class = "geom",
-                 type = "polygon",
-                 vert = theVertices,
-                 feat = theFeatures,
-                 group = theGroups,
-                 window = theWindow,
-                 scale = "absolute",
-                 crs = as.character(projection),
-                 history = list(paste0("geometry was created as 'polygon'.")))
 
   invisible(theGeom)
 }
@@ -238,18 +203,18 @@ gs_polygon <- function(anchor = NULL, window = NULL, template = NULL, features =
 #'   \code{regular = TRUE}.
 #' @export
 
-gs_triangle <- function(anchor = NULL, window = NULL, template = NULL,
+gs_triangle <- function(anchor = NULL, window = NULL, sketch = NULL,
                         features = 1, fixed = FALSE, ...){
 
-  if(is.null(anchor) & is.null(template)){
-    stop("please provide either 'anchor' or 'template'.")
+  if(is.null(anchor) & is.null(sketch)){
+    stop("please provide anchor values.")
   }
   assertDataFrame(window, types = "numeric", any.missing = FALSE, ncols = 2, null.ok = TRUE)
   assertIntegerish(features, len = 1, lower = 1)
 
   theGeom <- gs_polygon(anchor = anchor,
                         window = window,
-                        template = template,
+                        sketch = sketch,
                         features = features,
                         vertices = 3,
                         regular = TRUE,
@@ -263,18 +228,18 @@ gs_triangle <- function(anchor = NULL, window = NULL, template = NULL,
 #'   \code{regular = TRUE}.
 #' @export
 
-gs_square <- function(anchor = NULL, window = NULL, template = NULL,
+gs_square <- function(anchor = NULL, window = NULL, sketch = NULL,
                       features = 1, fixed = FALSE, ...){
 
-  if(is.null(anchor) & is.null(template)){
-    stop("please provide either 'anchor' or 'template'.")
+  if(is.null(anchor) & is.null(sketch)){
+    stop("please provide anchor values.")
   }
   assertDataFrame(window, types = "numeric", any.missing = FALSE, ncols = 2, null.ok = TRUE)
   assertIntegerish(features, len = 1, lower = 1)
 
   theGeom <- gs_polygon(anchor = anchor,
                         window = window,
-                        template = template,
+                        sketch = sketch,
                         features = features,
                         vertices = 4,
                         regular = TRUE,
@@ -289,18 +254,18 @@ gs_square <- function(anchor = NULL, window = NULL, template = NULL,
 #'   the two given opposing corners.
 #' @export
 
-gs_rectangle <- function(anchor = NULL, window = NULL, template = NULL,
+gs_rectangle <- function(anchor = NULL, window = NULL, sketch = NULL,
                          features = 1, ...){
 
-  if(is.null(anchor) & is.null(template)){
-    stop("please provide either 'anchor' or 'template'.")
+  if(is.null(anchor) & is.null(sketch)){
+    stop("please provide anchor values.")
   }
   assertDataFrame(window, types = "numeric", any.missing = FALSE, ncols = 2, null.ok = TRUE)
   assertIntegerish(features, len = 1, lower = 1)
 
   theGeom <- gs_polygon(anchor = anchor,
                         window = window,
-                        template = template,
+                        sketch = sketch,
                         features = features,
                         vertices = 2,
                         ...)
@@ -324,18 +289,18 @@ gs_rectangle <- function(anchor = NULL, window = NULL, template = NULL,
 #'   \code{regular = TRUE}.
 #' @export
 
-gs_hexagon <- function(anchor = NULL, window = NULL, template = NULL,
+gs_hexagon <- function(anchor = NULL, window = NULL, sketch = NULL,
                        features = 1, fixed = FALSE, ...){
 
-  if(is.null(anchor) & is.null(template)){
-    stop("please provide either 'anchor' or 'template'.")
+  if(is.null(anchor) & is.null(sketch)){
+    stop("please provide anchor values.")
   }
   assertDataFrame(window, types = "numeric", any.missing = FALSE, ncols = 2, null.ok = TRUE)
   assertIntegerish(features, len = 1, lower = 1)
 
   theGeom <- gs_polygon(anchor = anchor,
                         window = window,
-                        template = template,
+                        sketch = sketch,
                         features = features,
                         vertices = 6,
                         regular = TRUE,
