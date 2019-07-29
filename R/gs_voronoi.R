daa8f424da04d0430e7afa2b4773a900ccc1d10b
Steffen Ehrmann <steffen.science@funroll-loops.de>
2019-07-30 00:46:45 +0300

    update function for new sketch procedure

--- R/gs_voronoi.R
+++ R/gs_voronoi.R
@@ -6,9 +6,9 @@
 #' @param window [\code{data.frame(1)}]\cr in case the reference window deviates
 #'   from the bounding box of \code{anchor} (minimum and maximum values),
 #'   specify this here.
-#' @param template [\code{RasterLayer(1)} | \code{matrix(1)}]\cr Gridded object
-#'   that serves as template to sketch the tiling.
 #' @param features [\code{integerish(1)}]\cr number of tiles to create.
+#' @param sketch [\code{RasterLayer(1)} | \code{matrix(1)}]\cr Gridded object
+#'   that serves as template to sketch the tiling.
 #' @param ... [various]\cr graphical parameters to \code{\link{gt_locate}}, in case
 #'   the tiling is sketched; see \code{\link{gpar}}.
 #' @return An invisible \code{geom}.
@@ -27,11 +27,8 @@
 #'
 #' \dontrun{
 #'
-#' input <- gtRasters$continuous
-#'
-#' # create voronoi polygons interactively
-#' tiles <- gs_voronoi(template = input)
-#' visualise(geom = tiles, linecol = "orange", new = FALSE)
+#' tiles <- gs_voronoi(sketch = gtRasters$continuous)
+#' visualise(geom = tiles, new = FALSE)
 #' }
 #' @importFrom checkmate testDataFrame assertNames testClass testNull
 #'   assertDataFrame assert assertIntegerish
@@ -40,16 +37,16 @@
 #' @importFrom deldir deldir tile.list
 #' @export
 
-gs_voronoi <- function(anchor = NULL, window = NULL, template = NULL, features = 3,
+gs_voronoi <- function(anchor = NULL, window = NULL, features = 3, sketch = NULL,
                        ...){
 
   # check arguments
   anchor <- .testAnchor(x = anchor, ...)
   theWindow <- .testWindow(x = window, ...)
-  template <- .testTemplate(x = template, ...)
+  assertIntegerish(features, len = 1, lower = 1)
 
-  if(is.null(anchor) & is.null(template)){
-    stop("please provide either 'anchor' or 'template'.")
+  if(is.null(anchor) & is.null(sketch)){
+    stop("please provide anchor values.")
   }
   if(!is.null(anchor)){
     if(anchor$type == "geom"){
@@ -60,53 +57,41 @@ gs_voronoi <- function(anchor = NULL, window = NULL, template = NULL, features =
       }
     }
   }
-  assertIntegerish(features, len = 1, lower = 1)
 
-  # get some raster properties
-  if(!is.null(template)){
-    if(template$type == "RasterLayer"){
-      tempName <- names(template$obj)
-      dims <- dim(template$obj)
-      projection <- getCRS(x = template$obj)
-    } else{
-      tempName <- "layer"
-      dims <- dim(template$obj)
-      projection <- NA
-    }
+  # sketch the geometry
+  if(!is.null(sketch)){
+
+    template <- .testTemplate(x = sketch, ...)
+    theGeom <- gt_sketch(template = template$obj,
+                         shape = "point",
+                         features = features,
+                         ...)
+    tempAnchor <- theGeom@vert
+    theWindow <- theGeom@window
+
   } else{
-    tempName <- "layer"
-    projection <- NA
-  }
+    if(anchor$type == "geom"){
 
-  # if anchor does not exists, make it
-  if(is.null(anchor)){
+      if(is.null(theWindow)){
+        theWindow <- anchor$obj@window
+      }
+      tempAnchor <- anchor$obj@vert
+      theFeatures <- anchor$obj@feat
+      theGroups <- anchor$obj@group
+      projection <- getCRS(x = anchor$obj)
 
-    message(paste0("please click the ", features, " vertices."))
-    visualise(raster = template$obj)
-    theClicks <- gt_locate(samples = features, panel = tempName, silent = TRUE, ...)
-    theWindow <- tibble(x = c(0, dims[2], dims[2], 0, 0),
-                        y = c(0, 0, dims[1], dims[1], 0))
-    tempAnchor <- tibble(x = theClicks$x,
-                         y = theClicks$y,
-                         fid = 1:features)
-    theFeatures <- tibble(fid = unique(tempAnchor$fid), gid = unique(tempAnchor$fid))
-    theGroups <- tibble(gid = unique(tempAnchor$fid))
+    } else if(anchor$type == "df"){
 
-  } else if(anchor$type == "geom"){
-    if(is.null(theWindow)){
-      theWindow <- anchor$obj@window
+      if(is.null(theWindow)){
+        theWindow = tibble(x = c(min(anchor$obj$x), max(anchor$obj$x), max(anchor$obj$x), min(anchor$obj$x), min(anchor$obj$x)),
+                           y = c(min(anchor$obj$y), min(anchor$obj$y), max(anchor$obj$y), max(anchor$obj$y), min(anchor$obj$y)))
+      }
+      tempAnchor <- anchor$obj
+      theFeatures = tibble(fid = seq_along(tempAnchor$x), gid = seq_along(tempAnchor$x))
+      theGroups = tibble(gid = seq_along(tempAnchor$x))
+      projection <- NA
+
     }
-    tempAnchor <- anchor$obj@vert
-    theFeatures <- anchor$obj@feat
-    theGroups <- anchor$obj@group
-  } else if(anchor$type == "df"){
-    if(is.null(theWindow)){
-      theWindow = tibble(x = c(min(anchor$obj$x), max(anchor$obj$x), max(anchor$obj$x), min(anchor$obj$x), min(anchor$obj$x)),
-                         y = c(min(anchor$obj$y), min(anchor$obj$y), max(anchor$obj$y), max(anchor$obj$y), min(anchor$obj$y)))
-    }
-    tempAnchor <- anchor$obj
-    theFeatures = tibble(fid = seq_along(tempAnchor$x), gid = seq_along(tempAnchor$x))
-    theGroups = tibble(gid = seq_along(tempAnchor$x))
   }
 
   temp <- deldir(as.data.frame(tempAnchor), rw = c(min(theWindow$x), max(theWindow$x), min(theWindow$y), max(theWindow$y)), suppressMsge = TRUE)
@@ -124,17 +109,15 @@ gs_voronoi <- function(anchor = NULL, window = NULL, template = NULL, features =
     }
   }
 
-  out <- new(Class = "geom",
-             type = "polygon",
-             vert = theVertices,
-             # feat = tibble(fid = unique(theVertices$fid), gid = unique(theVertices$fid)),
-             # group = tibble(gid = unique(theVertices$fid)),
-             feat = theFeatures,
-             group = theGroups,
-             window = theWindow,
-             scale = "absolute",
-             crs = as.character(projection),
-             history = list(paste0("geometry was created as voronoi 'polygon'.")))
+  theGeom <- new(Class = "geom",
+                 type = "polygon",
+                 vert = theVertices,
+                 feat = theFeatures,
+                 group = theGroups,
+                 window = theWindow,
+                 scale = "absolute",
+                 crs = as.character(projection),
+                 history = list(paste0("geometry was created as voronoi 'polygon'.")))
 
-  invisible(out)
+  invisible(theGeom)
 }
