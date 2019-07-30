29f7466df0a8cdbf8e2531e955f5c3356763efda
Steffen Ehrmann <steffen.science@funroll-loops.de>
2019-07-30 19:53:26 +0300

    finetune tests

--- tests/testthat/test_gt_locate.R
+++ tests/testthat/test_gt_locate.R
@@ -35,3 +35,19 @@ test_that("identifying in a raster works", {
   expect_data_frame(x = output)
   expect_names(names(output), identical.to = c("id", "x", "y", "value", "colour"))
 })
+
+test_that("the correct warnings are printed", {
+  visualise(gtRasters$continuous)
+  expect_warning(object = gt_locate(identify = TRUE, panel = "categorical"), regexp = "the specified panel did not match any of the existing panels, please select locations in the first panel.")
+
+  visualise(gtRasters)
+  expect_warning(object = gt_locate(), regexp = "please select locations in the first panel.")
+})
+
+test_that("the correct errors are printed", {
+  dev.off()
+  expect_error(object = gt_locate(), regexp = "please create a plot with geometr::visualise()")
+
+  plot(c(1:10))
+  expect_error(object = gt_locate(), regexp = "please create a plot with geometr::visualise()")
+})
