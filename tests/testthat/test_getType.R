4c34ef69e70022af83542016274b4478b9f0791e
Steffen Ehrmann <steffen.science@funroll-loops.de>
2019-07-30 23:38:45 +0300

    new tests

---
+++ tests/testthat/test_getType.R
@@ -0,0 +1,32 @@
+library(checkmate)
+library(testthat)
+context("getType")
+
+
+test_that("getType of a 'geom'", {
+  output <- getType(x = gtGeoms$polygon)
+
+  expect_character(x = output, len = 2)
+  expect_true(all(output %in% c("vector", "polygon")))
+})
+
+test_that("getType of a Spatial* object", {
+  output <- getType(x = gtSP$SpatialPolygons)
+
+  expect_character(x = output, len = 2)
+  expect_true(all(output %in% c("vector", "SpatialPolygons")))
+})
+
+test_that("getType of an sf object", {
+  output <- getType(x = gtSF$polygon)
+
+  expect_character(x = output, len = 2)
+  expect_true(all(output %in% c("vector", "POLYGON")))
+})
+
+test_that("getType of a RasterLayer", {
+  output <- getType(x = gtRasters$continuous)
+
+  expect_character(x = output, len = 2)
+  expect_true(all(output %in% c("raster", "RasterLayer")))
+})
--- tests/testthat/test_gt_sketch.R
+++ tests/testthat/test_gt_sketch.R
@@ -3,3 +3,29 @@ library(checkmate)
 context("gt_sketch")
 
 
+test_that("sketch a point geometry", {
+  output <- gt_sketch(template = gtRasters$categorical, shape = "point")
+
+  expect_class(x = output, classes = "geom")
+  expect_true(object = output@type == "point")
+})
+
+test_that("sketch a line geometry", {
+  output <- gt_sketch(template = gtRasters$categorical, shape = "line")
+
+  expect_class(x = output, classes = "geom")
+  expect_true(object = output@type == "line")
+})
+
+test_that("sketch a polygon geometry", {
+  output <- gt_sketch(template = gtRasters$categorical, shape = "polygon")
+
+  expect_class(x = output, classes = "geom")
+  expect_true(object = output@type == "polygon")
+
+  # test regular polygons
+  output <- gt_sketch(template = gtRasters$categorical, shape = "polygon", regular = TRUE, fixed = FALSE)
+
+  expect_class(x = output, classes = "geom")
+  expect_true(object = output@type == "polygon")
+})
