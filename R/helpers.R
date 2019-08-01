04b7a667bab0076429b5c177ebd3f37f13daa25f
Steffen Ehrmann <steffen.science@funroll-loops.de>
2019-08-01 19:12:48 +0300

    new function .updateVertices() that checks consistency of vertices

--- R/helpers.R
+++ R/helpers.R
@@ -195,28 +195,128 @@ makeLayout <- function(x = NULL, window = NULL, theme = gtTheme, image = FALSE, 
 
 }
 
-#' Update the window slot
+#' Update the window
 #'
 #' Set the extent of a window to smaller/larger values, if the vertices would be
 #' beyond the window otherwise.
-#' @param geom [\code{geom}]\cr a geom for which a new window should be derived.
-#' @param window [\code{tibble(1)}]\cr the old window.
+#' @param input [\code{data.frame(1)}]\cr a table of vertices for which a new window should be derived.
+#' @param window [\code{data.frame(1)}]\cr the old window.
+#' @return A new window that has the extent of \code{input}.
 #' @export
 
-.updateWindow <- function(geom = NULL, window = NULL){
-  if(min(geom$x) < min(window$x)){
-    window$x[which.min(window$x)] <- min(geom$x)
+.updateWindow <- function(input = NULL, window = NULL){
+
+  # check arguments
+  names(input) <- tolower(names(input))
+  assertNames(x = names(input), must.include = c("x", "y"))
+  assertDataFrame(x = input, min.rows = 2)
+  names(window) <- tolower(names(window))
+  assertNames(x = names(window), must.include = c("x", "y"))
+  assertDataFrame(x = window, min.rows = 2)
+
+  if(min(input$x) < min(window$x)){
+    window$x[which.min(window$x)] <- min(input$x)
   }
-  if(max(geom$x) > max(window$x)){
-    window$x[which.max(window$x)] <- max(geom$x)
+  if(max(input$x) > max(window$x)){
+    window$x[which.max(window$x)] <- max(input$x)
   }
-  if(min(geom$y) < min(window$y)){
-    window$y[which.min(window$y)] <- min(geom$y)
+  if(min(input$y) < min(window$y)){
+    window$y[which.min(window$y)] <- min(input$y)
   }
-  if(max(geom$y) > max(window$y)){
-    window$y[which.max(window$y)] <- max(geom$y)
+  if(max(input$y) > max(window$y)){
+    window$y[which.max(window$y)] <- max(input$y)
   }
   return(window)
+}
+
+#' Update the vertices
+#'
+#' Set the verties in a table so that they are valid for a geom.
+#' @param input [\code{data.frame(1)}]\cr a table of vertices which should be
+#'   brought into the correct form.
+#' @param limit [\code{data.frame(1)}]\cr a table of vertices that outline a
+#'   hull beyond which vertices are not valid.
+#' @importFrom dplyr group_by mutate distinct ungroup
+#' @export
+
+.updateVertices <- function(input = NULL, limit = NULL){
+
+  # check arguments
+  names(input) <- tolower(names(input))
+  assertNames(x = names(input), must.include = c("x", "y"), subset.of = c("x", "y", "fid"))
+  assertDataFrame(x = input, min.rows = 2)
+
+  # if no fid is specified, treat it as if all vertices are part of the same feature
+  if(!"fid" %in% names(input)){
+    input <- bind_cols(input, fid = 1)
+  }
+
+  for(i in unique(input$fid)){
+    temp <- input[input$fid == i,]
+    verts <- temp
+    verts$seq <- seq_along(temp$fid)
+
+    # determine the duplicated vertices that enclose other vertices
+    dups <- duplicated(temp, fromLast = TRUE) + duplicated(temp)
+    if(any(dups > 0)){
+      bounds <- verts[which(as.logical(dups)),]
+      bounds <- group_by(.data = bounds, x, y, fid)
+      bounds <- mutate(.data = bounds, min = min(seq), max = max(seq), seq = NULL)
+      bounds <- distinct(.data = bounds)
+      bounds <- ungroup(bounds)
+
+      # label vertices
+      lab <- el <- seq <- NULL
+      full <- seq(from = min(bounds$min), to = max(bounds$max), by = 1)
+      for(j in seq_along(bounds$fid)){
+        lab <- c(lab, seq(from = bounds[j, ]$min, bounds[j, ]$max, by = 1))
+        el <- c(el, rep(j, bounds[j, ]$max - bounds[j, ]$min + 1))
+      }
+      seq[lab] <- el
+      temp$ring <- seq
+      lastRing <- max(temp$ring, na.rm = TRUE)
+
+    } else {
+      temp$ring <- NA
+      lastRing <- 0
+    }
+
+    # get values that are not yet part of a closed ring
+    missingRing <- which(is.na(temp$ring))
+    ind <- c(missingRing[-1], tail(missingRing, 1)+1) - missingRing
+
+    # split up into list of separate rings
+    splitBy <- NULL
+    k <- 1
+    for(j in seq_along(ind)){
+      if(ind[j] > 1){
+        splitBy <- c(splitBy, k)
+        k <- k + 1
+      } else {
+        splitBy <- c(splitBy, k)
+      }
+    }
+    missingRings <- split(missingRing, splitBy)
+
+    # go through all rings and check them ...
+    newRings <- NULL
+    for(j in seq_along(missingRings)){
+      aRing <- temp[missingRings[[j]], ]
+      aRing$ring <- lastRing + 1
+
+      dup <- duplicated(aRing, fromLast = TRUE)
+      if(!dup[1]){
+        aRing <- add_row(aRing, x = aRing$x[1], y = aRing$y[1], fid = aRing$fid[1], ring = lastRing + 1)
+      }
+
+      newRings <- bind_rows(newRings, aRing)
+    }
+  }
+
+  out <- bind_rows(temp[!is.na(temp$ring), ], newRings)
+  out$ring <- NULL
+
+  return(out)
 }
 
 #' Test anchor for consistency
