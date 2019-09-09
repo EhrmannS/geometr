defe38e3c9f02f7321c5f5732d49548ad7ad5654
Steffen Ehrmann <steffen.ehrmann@idiv.de>
2019-09-09 10:43:39 +0200

    new function gt_translate

--- R/gt_translate.R
+++ R/gt_translate.R
@@ -2,3 +2,112 @@
 #'
 #' Translate \code{geom}s by adding a constant in x and y-dimension.
 #' @param [\code{geom(.)}]\cr the object to translate.
+#' @param x [\code{numeric(1)}]\cr the translation constant in x-dimension.
+#' @param y [\code{numeric(1)}]\cr the translation constant in y-dimension.
+#' @param fid [\code{integerish(.)}]\cr if only a subset of features shall be
+#'   rotated, specify that here.
+#' @param update [\code{logical(1)}]\cr whether or not to update the window slot
+#'   after rotation.
+#' @return Mathematically translated \code{geom}.
+#' @family geometry tools
+#' @examples
+#' # the original object
+#' coords <- data.frame(x = c(30, 60, 60, 40, 10, 40, 20),
+#'                      y = c(40, 40, 60, 70, 10, 20, 40),
+#'                      fid = c(1, 1, 1, 1, 2, 2, 2))
+#' window <- data.frame(x = c(0, 80),
+#'                      y = c(0, 80))
+#' aGeom <- gs_polygon(anchor = coords, window = window)
+#'
+#' # translate several geoms
+#' visualise(geom = gt_translate(geom = aGeom, x = 5, y = list(-10, 5)))
+#'
+#' # translate single geom
+#' visualise(geom = gt_translate(geom = aGeom, x = 5, fid = 1))
+#' @importFrom checkmate assertClass testList testNumeric assert
+#'   assertIntegerish assertLogical
+#' @importFrom tibble as_tibble
+#' @importFrom methods new
+#' @export
+
+gt_translate <- function(geom = NULL, x = NULL, y = NULL, fid = NULL,
+                         update = TRUE){
+
+  assertClass(geom, classes = "geom")
+  xIsList <- testList(x, types = "numeric", any.missing = FALSE)
+  xIsNumeric <- testNumeric(x, any.missing = FALSE, len = 1, null.ok = TRUE)
+  yIsList <- testList(y, types = "numeric", any.missing = FALSE)
+  yIsNumeric <- testNumeric(y, any.missing = FALSE, len = 1, null.ok = TRUE)
+  assert(xIsList, xIsNumeric)
+  assert(yIsList, yIsNumeric)
+  assertIntegerish(x = fid, any.missing = FALSE, null.ok = TRUE)
+  assertLogical(x = update, len = 1, any.missing = FALSE)
+
+  if(is.null(x)){
+    x <- 0
+  }
+  if(is.null(y)){
+    y <- 0
+  }
+
+  if(xIsNumeric){
+    x <- list(x)
+  }
+  if(yIsNumeric){
+    y <- list(y)
+  }
+
+  verts <- geom@vert
+  ids <- unique(verts$fid)
+
+  existsID <- !is.null(fid)
+  if(existsID){
+    doTranslate <- ids %in% fid
+  } else{
+    doTranslate <- rep(TRUE, length(ids))
+  }
+
+  if(length(x) != length(ids)){
+    x <- rep(x, length.out = length(ids))
+  }
+  if(length(y) != length(ids)){
+    y <- rep(y, length.out = length(ids))
+  }
+
+  temp <- NULL
+  for(i in seq_along(ids)){
+    tempCoords <- verts[verts$fid == ids[i],]
+    newCoords <- tempCoords
+
+    if(doTranslate[i]){
+      newCoords$x <- tempCoords$x + x[[i]]
+      newCoords$y <- tempCoords$y + y[[i]]
+    }
+
+    temp <- rbind(temp, newCoords)
+  }
+
+  if(update){
+    window <- .updateWindow(input = temp, window = geom@window)
+  } else {
+    window <- geom@window
+  }
+
+  if(length(ids) == 1){
+    newHistory <- paste0("geometry was translated")
+  } else {
+    newHistory <- paste0("geometries were translated")
+  }
+
+  out <- new(Class = "geom",
+             type = geom@type,
+             vert = as_tibble(temp),
+             feat = geom@feat,
+             group = geom@group,
+             window = window,
+             scale = geom@scale,
+             crs = geom@crs,
+             history = c(geom@history, list(newHistory)))
+
+  return(out)
+}
