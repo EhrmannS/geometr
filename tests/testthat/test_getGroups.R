4a1cbf6657dc10ce98e1db2ad309e9c14014e57a
Steffen Ehrmann <steffen.science@funroll-loops.de>
2019-11-11 00:31:21 +0100

    incl. getSubset into the respective getters

--- tests/testthat/test_getGroups.R
+++ tests/testthat/test_getGroups.R
@@ -28,6 +28,27 @@ test_that("getGroups of a 'geom'", {
   expect_names(x = names(output), permutation.of = c("gid"))
 })
 
+test_that("getGroups with subset of a 'geom'", {
+  coords <- data.frame(x = c(40, 70, 70, 50),
+                       y = c(40, 40, 60, 70),
+                       fid = c(1, 1, 2, 2))
+  input <- gs_polygon(anchor = coords)
+
+  # based on a condition
+  input <- setGroups(x = input, table = data.frame(gid = c(1, 2), attr = c("A", "B")))
+  output <- getGroups(x = input, attr == 'B')
+  expect_class(output, "geom")
+  expect_true(dim(output@group$geometry)[1] == 1)
+  expect_true(dim(output@group$geometry)[1] < dim(input@group$geometry)[1])
+
+  # based on a logical
+  subset <- c(TRUE, FALSE)
+  output <- getGroups(x = input, subset)
+  expect_class(output, "geom")
+  expect_true(dim(output@group$geometry)[1] == 1)
+  expect_true(dim(output@group$geometry)[1] < dim(input@group$geometry)[1])
+})
+
 test_that("getGroups of a Raster* object", {
 
   # test RasterLayer without attribute table
@@ -43,11 +64,16 @@ test_that("getGroups of a Raster* object", {
   expect_names(names(output), permutation.of = c("fid", "cover"))
 })
 
-test_that("getGroups returns a given attribute table", {
+test_that("getGroups returns a given raster attribute table", {
   input <- gtRasters$categorical
 
   output <- getGroups(input)
   expect_data_frame(output, any.missing = FALSE, nrows = 9, ncols = 2)
   expect_names(names(output), identical.to = c("fid", "cover"))
+})
+
+test_that("getGroups of any other object", {
+  output <- getGroups("bla")
+  expect_null(object = output)
 })
 
--- tests/testthat/test_getPoints.R
+++ tests/testthat/test_getPoints.R
@@ -1,4 +1,5 @@
 library(checkmate)
+library(testthat)
 library(sf)
 context("getPoints")
 
@@ -14,6 +15,26 @@ test_that("getPoints of a 'geom'", {
   output <- getPoints(x = input)
   expect_data_frame(output, any.missing = FALSE, nrows = 5, ncols = 3)
   expect_names(names(output), identical.to = c("x", "y", "fid"))
+})
+
+test_that("getPoints with subset of a 'geom'", {
+  coords <- data.frame(x = c(40, 70, 70, 50),
+                       y = c(40, 40, 60, 70),
+                       fid = c(1, 1, 2, 2))
+  input <- gs_polygon(anchor = coords)
+
+  # based on a condition
+  output <- getPoints(x = input, fid == 2)
+  expect_class(output, "geom")
+  expect_true(dim(output@point)[1] == 3)
+  expect_true(dim(output@point)[1] < dim(input@point)[1])
+
+  # based on a logical
+  subset <- c(TRUE, TRUE, FALSE, TRUE, TRUE, FALSE)
+  output <- getPoints(x = input, subset)
+  expect_class(output, "geom")
+  expect_true(dim(output@point)[1] == 4)
+  expect_true(dim(output@point)[1] < dim(input@point)[1])
 })
 
 test_that("getPoints of a Spatial* object", {
