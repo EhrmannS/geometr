960044451ee4351641fd0d8d65819ad2ae8e6ddc
Steffen Ehrmann <steffen.science@funroll-loops.de>
2020-01-19 19:26:04 +0100

    fix tests

--- tests/testthat/test_makeObject.R
+++ tests/testthat/test_makeObject.R
@@ -6,38 +6,38 @@ context("makeObject")
 
 
 test_that("make object from a geom", {
-  output <- makeObject(x = gtGeoms$polygon, window = NULL, theme = gtTheme)
+  output <- makeObject(x = list(gtGeoms$polygon), window = NULL, theme = gtTheme)
   expect_list(x = output, len = 7)
   expect_names(x = names(output), permutation.of = c("type", "name", "window", "out", "hasLegend", "params", "legend"))
 
   # with a modified theme
   myTheme <- gtTheme
   myTheme@legend$ascending <- FALSE
-  output <- makeObject(x = gtGeoms$polygon, window = NULL, theme = myTheme)
+  output <- makeObject(x = list(gtGeoms$polygon), window = NULL, theme = myTheme)
   expect_list(x = output, len = 7)
   expect_names(x = names(output), permutation.of = c("type", "name", "window", "out", "hasLegend", "params", "legend"))
 
   # a more complex geom
   nc <- st_read(system.file("shape/nc.shp", package="sf"))
-  output <- makeObject(x = gc_geom(input = nc), window = NULL, theme = gtTheme)
+  output <- makeObject(x = list(gc_geom(input = nc)), window = NULL, theme = gtTheme)
   expect_list(x = output, len = 7)
   expect_names(x = names(output), permutation.of = c("type", "name", "window", "out", "hasLegend", "params", "legend"))
 
   # when a deviating window is used
   input <- setWindow(x = gtGeoms$polygon, to = data.frame(x = c(0, 11), y = c(0, 11)))
-  expect_warning(object = output <- makeObject(x = input, window = NULL, theme = gtTheme), regexp = "some vertices are not within the plotting window.")
+  expect_warning(object = output <- makeObject(x = list(input), window = NULL, theme = gtTheme), regexp = "some vertices are not within the plotting window.")
   expect_list(x = output, len = 7)
   expect_names(x = names(output), permutation.of = c("type", "name", "window", "out", "hasLegend", "params", "legend"))
 
   input <- setWindow(x = gtGeoms$polygon, to = data.frame(x = c(3, 5), y = c(3, 5)))
-  expect_warning(object = output <- makeObject(x = input, window = NULL, theme = gtTheme), regexp = "no vertices are within the plotting window.")
+  expect_warning(object = output <- makeObject(x = list(input), window = NULL, theme = gtTheme), regexp = "no vertices are within the plotting window.")
   expect_list(x = output, len = 7)
   expect_names(x = names(output), permutation.of = c("type", "name", "window", "out", "hasLegend", "params", "legend"))
 })
 
 test_that("make object from a Raster", {
   # a categorical raster with colourtable
-  output <- makeObject(x = gtRasters$categorical, window = NULL, theme = gtTheme)
+  output <- makeObject(x = list(gtRasters$categorical), window = NULL, theme = gtTheme)
   expect_list(x = output, len = 10)
   expect_names(x = names(output), permutation.of = c("type", "name", "extent", "window", "rows", "cols", "hasLegend", "params", "legend", "values"))
 
@@ -49,44 +49,44 @@ test_that("make object from a Raster", {
   expect_names(x = names(output), permutation.of = c("type", "name", "extent", "window", "rows", "cols", "hasLegend", "params", "legend", "values"))
 
   # a continuous raster
-  output <- makeObject(x = gtRasters$continuous, window = NULL, theme = gtTheme)
+  output <- makeObject(x = list(gtRasters$continuous), window = NULL, theme = gtTheme)
   expect_list(x = output, len = 10)
   expect_names(x = names(output), permutation.of = c("type", "name", "extent", "window", "rows", "cols", "hasLegend", "params", "legend", "values"))
 
   # an image
   input <- RGB(gtRasters$continuous)
-  output <- makeObject(x = input, image = TRUE, window = NULL, theme = gtTheme)
+  output <- makeObject(x = list(input), image = TRUE, window = NULL, theme = gtTheme)
   expect_names(x = names(output), permutation.of = c("type", "name", "extent", "window", "rows", "cols", "hasLegend", "params", "legend", "values"))
 
   # errors
   # expect_error(object = makeObject(x = input, theme = gtTheme))
-  expect_error(object = makeObject(x = gtRasters$categorical, window = NULL, image = TRUE, theme = gtTheme))
+  expect_error(object = makeObject(x = list(gtRasters$categorical), window = NULL, image = TRUE, theme = gtTheme))
 })
 
 test_that("make object from a matrix", {
   # a matrix with hexadecimal colour values
   input1 <- matrix(c(rep('#000000', 4)), nrow = 2)
-  output <- makeObject(x = input1, window = NULL, image = TRUE, theme = gtTheme)
+  output <- makeObject(x = list(input1), window = NULL, image = TRUE, theme = gtTheme)
   expect_names(x = names(output), permutation.of = c("type", "name", "extent", "window", "rows", "cols", "hasLegend", "params", "legend", "values"))
 
   # an integer value matrix
   input2 <- matrix(c(rep(1, 4)), nrow = 2)
-  output <- makeObject(x = input2, window = NULL, theme = gtTheme)
+  output <- makeObject(x = list(input2), window = NULL, theme = gtTheme)
   expect_names(x = names(output), permutation.of = c("type", "extent", "window", "rows", "cols", "hasLegend", "params", "legend", "values"))
 
   # with a modified theme
   myTheme <- gtTheme
   myTheme@legend$ascending <- FALSE
-  output <- makeObject(x = input2, window = NULL, theme = myTheme)
+  output <- makeObject(x = list(input2), window = NULL, theme = myTheme)
   expect_names(x = names(output), permutation.of = c("type", "extent", "window", "rows", "cols", "hasLegend", "params", "legend", "values"))
 
   # errors
-  expect_error(object = makeObject(x = input1, theme = gtTheme))
-  expect_error(object = makeObject(x = input2, window = NULL, image = TRUE, theme = gtTheme))
+  expect_error(object = makeObject(x = list(input1), theme = gtTheme))
+  expect_error(object = makeObject(x = llist(input2), window = NULL, image = TRUE, theme = gtTheme))
 })
 
 test_that("make object from a Spatial", {
-  output <- makeObject(x = gtSP$SpatialPolygons, window = NULL, theme = gtTheme)
+  output <- makeObject(x = list(gtSP$SpatialPolygons), window = NULL, theme = gtTheme)
   expect_list(x = output, len = 7)
   expect_names(x = names(output), permutation.of = c("type", "name", "window", "out", "hasLegend", "params", "legend"))
 
@@ -94,7 +94,7 @@ test_that("make object from a Spatial", {
 })
 
 test_that("make object from a sf", {
-  output <- makeObject(x = gtSF$polygon, window = NULL, theme = gtTheme)
+  output <- makeObject(x = list(gtSF$polygon), window = NULL, theme = gtTheme)
   expect_list(x = output, len = 7)
   expect_names(x = names(output), permutation.of = c("type", "name", "window", "out", "hasLegend", "params", "legend"))
 
--- tests/testthat/test_show.R
+++ tests/testthat/test_show.R
@@ -7,7 +7,7 @@ test_that("geom with less than 9 attributes", {
   output <- capture.output(gtGeoms$polygon)
 
   expect_character(x = output, len = 10)
-  expect_true(output[2] == "            2 features | 15 points")
+  expect_true(output[2] == "            2 groups | 2 features | 15 points")
 })
 
 test_that("geom with all attribute tables", {
@@ -16,24 +16,24 @@ test_that("geom with all attribute tables", {
   input <- setGroups(x = input, table = data.frame(blubb = c(1:2)))
   output <- capture.output(input)
   expect_character(x = output, len = 12)
-  expect_true(output[2] == "            2 features | 15 points")
+  expect_true(output[2] == "            2 groups | 2 features | 15 points")
 })
 
 test_that("geom with single attribute tables", {
   input <- setFeatures(x = gtGeoms$polygon, table = data.frame(bla = c(1:2)))
   output <- capture.output(input)
   expect_character(x = output, len = 10)
-  expect_true(output[2] == "            2 features | 15 points")
+  expect_true(output[2] == "            2 groups | 2 features | 15 points")
 
   input <- setPoints(x = gtGeoms$polygon, table = data.frame(wat = c(1:15)))
   output <- capture.output(input)
   expect_character(x = output, len = 10)
-  expect_true(output[2] == "            2 features | 15 points")
+  expect_true(output[2] == "            2 groups | 2 features | 15 points")
 
   input <- setGroups(x = gtGeoms$polygon, table = data.frame(blubb = c(1:2)))
   output <- capture.output(input)
   expect_character(x = output, len = 10)
-  expect_true(output[2] == "            2 features | 15 points")
+  expect_true(output[2] == "            2 groups | 2 features | 15 points")
 })
 
 test_that("geom with crs", {
