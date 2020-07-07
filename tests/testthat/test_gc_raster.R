f0e5bf1f84a3a1af37b2dbebfe5f23fa6597c7fd
Steffen Ehrmann <steffen.science@funroll-loops.de>
2020-07-08 00:03:41 +0200

    remove meaningless test

--- tests/testthat/test_gc_raster.R
+++ tests/testthat/test_gc_raster.R
@@ -6,19 +6,9 @@ context("gc_raster")
 
 test_that("transform geom to raster", {
   # test a single RasterLayer
-  input <- gc_geom(input = gtRasters$categorical)
-
-  output <- gc_raster(input = input)
+  output <- gc_raster(input = gtGeoms$grid$categorical)
   expect_class(x = output, classes = "RasterLayer")
   expect_data_frame(x = output@data@attributes[[1]], any.missing = FALSE, nrows = 9, ncols = 2)
-
-  # test a RasterStack
-  input <- gc_geom(input = gtRasters)
-
-  output <- gc_raster(input = input)
-  expect_class(x = output, classes = "RasterStack")
-  expect_true(object = dim(output)[3] == 2)
-  expect_data_frame(x = output[[1]]@data@attributes[[1]], any.missing = FALSE, nrows = 9, ncols = 2)
 })
 
 test_that("errors when transforming a geom not of type 'grid'", {
