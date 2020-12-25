33153ab200affc85696b8a8ca530657e649537d6
Steffen Ehrmann <steffen.ehrmann@idiv.de>
2020-12-25 13:20:18 +0100

    revise function so that it works for all classes, also make scaling more explicit by removing "to" argument

--- R/gt_scale.R
+++ R/gt_scale.R
@@ -2,12 +2,9 @@
 #'
 #' Scale the vertex values of \code{geom}s to a values range or so that they are
 #' either relative to the \code{@window} slot, or absolute values.
-#' @param geom [\code{geom(.)}]\cr the object to be scaled.
+#' @param x [\code{geometric object(1)}]\cr the object to be scaled.
 #' @param range [\code{list(2)}]\cr vector of length two for both of the
 #'   \code{x} and \code{y} dimension to which the values should be scaled.
-#' @param to [\code{character(1)}]\cr the scale to which the coordinates should
-#'   be transformed; possible are \code{"relative"} and \code{"absolute"};
-#'   ignored in case \code{range != NULL}.
 #' @return Scaled \code{geom}.
 #' @family geometry tools
 #' @examples
@@ -19,96 +16,64 @@
 #' aGeom <- gs_polygon(anchor = coords, window = window)
 #'
 #' # change to relative scale and back to absolute
-#' (relCoords <- gt_scale(geom = aGeom, to = "relative"))
-#' gt_scale(geom = relCoords, to = "absolute")
+#' (relCoords <- gt_scale(geom = aGeom, range = data.frame(x = c(0, 1), y = c(0, 1))))
+#' gt_scale(geom = relCoords, range = window)
 #'
 #' # scale to another range
-#' gt_scale(geom = aGeom, range = list(x = c(0, 100), y = c(10, 90)))
+#' gt_scale(geom = aGeom, range = tibble(x = c(0, 100), y = c(10, 90)))
 #'
 #' @importFrom checkmate testList assertNames assertChoice
 #' @importFrom tibble as_tibble
 #' @importFrom methods new
 #' @export
 
-gt_scale <- function(geom, range = NULL, to = "relative"){
+gt_scale <- function(x, range = NULL){
 
-  assertClass(geom, classes = "geom")
-  existsRange <- testTRUE(!is.null(range))
-  if(existsRange){
-    assertList(range, len = 2, names = "named")
-    assertNames(names(range), permutation.of = c("x", "y"))
-    assertIntegerish(range$x, len = 2, any.missing = FALSE)
-    assertIntegerish(range$y, len = 2, any.missing = FALSE)
-    to <- "relative"
-  } else{
-    to <- assertChoice(x = to, choices = c("relative", "absolute"))
+  assertDataFrame(x = range, types = "numeric", any.missing = FALSE, ncols = 2)
+  assertNames(names(range), permutation.of = c("x", "y"))
+
+  thePoints <- getPoints(x = x)
+  theType <- getType(x = x)[1]
+
+  minX <- min(thePoints$x)
+  maxX <- max(thePoints$x)
+  minY <- min(thePoints$y)
+  maxY <- max(thePoints$y)
+
+  if(minX == maxX){
+    stop("I can't scale a 'geom' that has equal minimum and maximum x values of the window.")
   }
-
-  point <- getPoints(x = geom)
-  window <- getWindow(x = geom)
-
-  out <- NULL
-  if(to == "relative"){
-    if(existsRange){
-      newScale <- "an 'absolute'"
-      rangeX <- range$x
-      rangeY <- range$y
-    } else{
-      newScale <- "a 'relative'"
-      rangeX <- c(0, 1)
-      rangeY <- c(0, 1)
-    }
-    minX <- min(window$x)
-    maxX <- max(window$x)
-    minY <- min(window$y)
-    maxY <- max(window$y)
-
-    if(minX == maxX){
-      stop("I can't scale a 'geom' that has equal minimum and maximum x values of the window.")
-    }
-    if(minY == maxY){
-      stop("I can't scale a 'geom' that has equal minimum and maximum y values of the window.")
-    }
-  } else{
-    newScale <- "an 'absolute'"
-    rangeX <- c(min(window$x), max(window$x))
-    rangeY <- c(min(window$y), max(window$y))
-    minX <- 0
-    maxX <- 1
-    minY <- 0
-    maxY <- 1
+  if(minY == maxY){
+    stop("I can't scale a 'geom' that has equal minimum and maximum y values of the window.")
   }
+  thePoints$x <- (thePoints$x - minX) * (max(range$x) - min(range$x)) / (maxX - minX) +  min(range$x)
+  thePoints$y <- (thePoints$y - minY) * (max(range$y) - min(range$y)) / (maxY - minY) +  min(range$y)
 
-  temp <- point
-  temp$x <- (temp$x - minX) * (rangeX[2] - rangeX[1]) / (maxX - minX) + rangeX[1]
-  temp$y <- (temp$y - minY) * (rangeY[2] - rangeY[1]) / (maxY - minY) + rangeY[1]
-  out <- rbind(out, temp)
-
-  if(existsRange){
+  if(!is.null(range)){
     window <- as.data.frame(range)
   }
 
-  if(geom@type == "grid"){
-    theFeatures <- getFeatures(x = geom)
-    theGroups <- getGroups(x = geom)
+  if(theType == "grid"){
+    theFeatures <- getFeatures(x = x)
+    theGroups <- getGroups(x = x)
   } else {
-    theFeatures <- list(geometry = getFeatures(x = geom))
-    theGroups <- list(geometry = getGroups(x = geom))
+    theFeatures <- list(geometry = getFeatures(x = x))
+    theGroups <- list(geometry = getGroups(x = x))
   }
+
+  # make history
+  hist <- paste0("coordinate values were rescaled between [", paste0(range$x, collapse = " "), "] (x) and [",  paste0(range$y, collapse = " "), "] (y)")
 
   # make new geom
   out <- new(Class = "geom",
-             type = getType(x = geom)[2],
-             point = as_tibble(out),
+             type = theType,
+             point = thePoints,
              feature = theFeatures,
              group = theGroups,
-             window = window,
-             scale = to,
-             crs = getCRS(x = geom),
-             history = c(getHistory(x = geom)))
-
-  # assign history
-  out <- setHistory(x = out, history = paste0("vertex values were scaled to ", newScale, " scale."))
+             window = getWindow(x = x),
+             scale = "absolute",
+             crs = getCRS(x = x),
+             history =c(getHistory(x = x), list(hist)))
 
   return(out)
 }
