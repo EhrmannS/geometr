8e21aa11c8a67a45b0f421c08a95db016a494e3c
Steffen Ehrmann <steffen.ehrmann@idiv.de>
2021-03-15 13:26:17 +0100

    harmonise making grobs for all params

--- R/makeGrob.R
+++ R/makeGrob.R
@@ -1,12 +1,14 @@
 #' Make the grob of a plot
 #'
 #' @param x the object to transform to class \code{grob}.
+#' @param plotParams [\code{named list(.)}]\cr new plotting parameters specified
+#'   via the quick options in \code{\link{visualise}}.
 #' @param theme [\code{gtTheme(1)}]\cr the theme from which to take parameters.
 #' @return Depending on the provided geometry either a \code{\link{pointsGrob}},
 #'   a \code{\link{polylineGrob}}, a \code{\link{pathGrob}} or a
 #'   \code{\link{rasterGrob}}.
 #' @importFrom rlang exprs rep_along
-#' @importFrom grDevices colorRampPalette colors
+#' @importFrom grDevices colorRampPalette colors rgb
 #' @importFrom stats setNames
 #' @importFrom tibble as_tibble
 #' @importFrom checkmate assertNames assertSubset assertList
@@ -14,13 +16,125 @@
 #' @importFrom grid gpar unit pointsGrob gList pathGrob polylineGrob clipGrob
 #'   rasterGrob
 
-.makeGrob <- function(x, theme = gtTheme){
+.makeGrob <- function(x, plotParams, theme = gtTheme){
 
-  # capture display arguments
   featureType <- getType(x = x)
-  params <- theme@parameters
 
   if(featureType[1] != "grid") {
+
+    params <- list(linecol = "black",
+                   fillcol = NA,
+                   linetype = "solid",
+                   linewidth = 1,
+                   pointsize = 0.5,
+                   pointsymbol = 20)
+
+    # # select only 'plotParams' that are part of the valid parameters.
+    tempArgs <- plotParams[names(plotParams) %in% names(params)]
+
+    # if the parameter to scale has not beend defined as quick parameter, add it to 'tempArgs'
+    if(!theme@scale$param %in% names(plotParams) & !is.na(theme@scale$param)){
+      tempArgs <- c(tempArgs, setNames(list(theme@scale$to), theme@scale$param))
+    }
+
+    # process parameters that shall be changed
+    for(i in seq_along(tempArgs)){
+
+      # determine value and name of the i-th display argument
+      theVar <- tempArgs[[i]]
+      theParam <- names(tempArgs)[i]
+      # assertChoice(x = theme@scale$param, choices = names(params))
+      pos <- which(names(params) %in% theParam)
+
+      # if the argument is a colour argument, construct a color ramp from two or more values
+      if(theParam %in% c("linecol", "fillcol")){
+
+        items <- suppressMessages(gt_pull(obj = x, var = theVar))
+
+        if(!is.null(theme@scale$bins)){
+          thebins <- theme@scale$bins
+        } else {
+          thebins <- length(items)
+        }
+
+        if(is.null(items)){
+          cols <- theVar
+          if(!any(as.character(cols) %in% colors()) & !any(grepl(pattern = "\\#(.{6,8})", x = cols))){
+            stop(paste0("'", cols, "' was neither found as column in the object to plot, nor is it a valid colour."))
+          }
+          tempOut <- colorRampPalette(colors = cols)(length(theVar))
+        } else {
+          cols <- theme@parameters$colours
+          allColours <- colorRampPalette(colors = cols)(thebins)
+
+          valCuts <- rank(items, ties.method = "min")
+          tempOut <- allColours[valCuts]
+        }
+
+        if(!is.null(theme@parameters$missingcol)){
+          tempOut[is.na(tempOut)] <- theme@parameters$missingcol
+        }
+
+      } else if(theParam %in% c("linewidth", "pointsize")){
+
+        items <- suppressMessages(gt_pull(obj = x, var = theVar))
+        num <- suppressWarnings(as.numeric(as.character(theVar)))
+
+        if(!is.null(theme@scale$bins)){
+          thebins <- theme@scale$bins
+        } else {
+          thebins <- length(items)
+        }
+
+        if(is.null(items)){
+          if(is.na(num)){
+            stop(paste0("'", theVar, "' was neither found as column in the object to plot, nor is it a valid ", theParam, "."))
+          }
+          tempOut <- num
+        } else {
+          allSizes <- seq(from = min(theme@parameters[[theParam]], na.rm = TRUE),
+                          to = max(theme@parameters[[theParam]], na.rm = TRUE),
+                          length.out = thebins)
+
+          if(is.null(items)){
+            tempOut <- rep(num, length(allSizes))
+          } else {
+            valCuts <- rank(items, ties.method = "min")
+            tempOut <- allSizes[valCuts]
+          }
+        }
+
+      } else if(theParam %in% c("pointsymbol", "linetype")){
+
+        items <- suppressMessages(gt_pull(obj = x, var = theVar))
+        num <- suppressWarnings(as.numeric(as.character(theVar)))
+
+        if(!is.null(theme@scale$bins)){
+          thebins <- theme@scale$bins
+        } else {
+          thebins <- length(items)
+        }
+
+        if(is.null(items)){
+          if(is.na(num)){
+            stop(paste0("'", theVar, "' was neither found as column in the object to plot, nor is it a valid ", theParam, "."))
+          }
+          tempOut <- num
+        } else {
+          allSymbols <- theme@parameters[[theParam]]
+
+          if(is.null(items)){
+            tempOut <- rep(num, length(allSymbols))
+          } else {
+            valCuts <- rank(items, ties.method = "min")
+            tempOut <- allSymbols[valCuts]
+          }
+        }
+
+      }
+
+      params[[pos]] <- tempOut
+    }
 
     # rescale values between 0 and 1
     x <- gt_scale(obj = x, range = tibble(x = c(0, 1), y = c(0, 1)))
@@ -75,9 +189,20 @@
 
   } else {
 
-    theRows <- theme@scale$rows
-    theCols <- theme@scale$cols
-    theColours <- params$fillcol
+    vals <- getFeatures(x = x)$values
+
+    if(testCharacter(x = vals, pattern = "\\#(.{6,8})")){
+      theColours <- as.vector(vals)
+    } else {
+      items <- sort(gt_pull(obj = x, var = theme@scale$to))
+      breaksTemp <- c(items[1]-1, items)
+      valCuts <- cut(vals, breaks = breaksTemp, include.lowest = TRUE)
+
+      cols <- theme@parameters$colours
+      allColours <- colorRampPalette(colors = cols)(theme@scale$bins)
+
+      theColours <- allColours[valCuts]
+    }
 
     out <- rasterGrob(x = unit(0, "npc"),
                       y = unit(0, "npc"),
@@ -85,7 +210,7 @@
                       height = unit(1, "npc"),
                       hjust = 0,
                       vjust = 0,
-                      image = matrix(data = theColours, nrow = theRows, ncol = theCols, byrow = TRUE),
+                      image = matrix(data = theColours, nrow = getRows(x = x), ncol = getCols(x = x), byrow = TRUE),
                       name = "theRaster",
                       interpolate = FALSE)
   }
@@ -95,5 +220,6 @@
   }
 
   return(out)
+
 }
 
