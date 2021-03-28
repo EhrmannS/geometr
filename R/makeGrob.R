f3e929f9a6ecd37c3631e490ee001c74ce4407d5
Steffen Ehrmann <steffen.ehrmann@idiv.de>
2021-03-28 17:54:03 +0200

    only make grob if theme@box$plot is TRUE

--- R/makeGrob.R
+++ R/makeGrob.R
@@ -18,199 +18,204 @@
 
 .makeGrob <- function(x, plotParams, theme = gtTheme){
 
-  featureType <- getType(x = x)
+  if(theme@box$plot){
 
-  if(featureType[1] != "grid") {
+    featureType <- getType(x = x)
+    if(featureType[1] != "grid") {
 
-    params <- list(linecol = "black",
-                   fillcol = NA,
-                   linetype = "solid",
-                   linewidth = 1,
-                   pointsize = 0.5,
-                   pointsymbol = 20)
+      params <- list(linecol = "black",
+                     fillcol = NA,
+                     linetype = "solid",
+                     linewidth = 1,
+                     pointsize = 0.5,
+                     pointsymbol = 20)
 
-    # # select only 'plotParams' that are part of the valid parameters.
-    tempArgs <- plotParams[names(plotParams) %in% names(params)]
+      # # select only 'plotParams' that are part of the valid parameters.
+      tempArgs <- plotParams[names(plotParams) %in% names(params)]
 
-    # if the parameter to scale has not beend defined as quick parameter, add it to 'tempArgs'
-    if(!theme@scale$param %in% names(plotParams) & !is.na(theme@scale$param)){
-      tempArgs <- c(tempArgs, setNames(list(theme@scale$to), theme@scale$param))
-    }
-
-    # process parameters that shall be changed
-    for(i in seq_along(tempArgs)){
-
-      # determine value and name of the i-th display argument
-      theVar <- tempArgs[[i]]
-      theParam <- names(tempArgs)[i]
-      pos <- which(names(params) %in% theParam)
-
-      items <- suppressMessages(gt_pull(obj = x, var = theVar))
-      num <- suppressWarnings(as.numeric(as.character(theVar)))
-
-      # if the argument is a colour argument, construct a color ramp from two or more values
-      if(theParam %in% c("linecol", "fillcol")){
-
-        if(!is.null(theme@scale$bins)){
-          thebins <- theme@scale$bins
-        } else {
-          thebins <- length(items)
-        }
-
-        if(is.null(items)){
-          cols <- theVar
-          if(!any(as.character(cols) %in% colors()) & !any(grepl(pattern = "\\#(.{6,8})", x = cols))){
-            stop(paste0("'", cols, "' was neither found as column in the object to plot, nor is it a valid colour."))
-          }
-          tempOut <- colorRampPalette(colors = cols)(length(theVar))
-        } else {
-          cols <- theme@parameters$colours
-          allColours <- colorRampPalette(colors = cols)(thebins)
-
-          valCuts <- rank(items, ties.method = "min")
-          tempOut <- allColours[valCuts]
-        }
-
-        if(!is.null(theme@parameters$missingcol)){
-          tempOut[is.na(tempOut)] <- theme@parameters$missingcol
-        }
-
-      } else if(theParam %in% c("linewidth", "pointsize")){
-
-        if(!is.null(theme@scale$bins)){
-          thebins <- theme@scale$bins
-        } else {
-          thebins <- length(items)
-        }
-
-        if(is.null(items)){
-          if(is.na(num)){
-            stop(paste0("'", theVar, "' was neither found as column in the object to plot, nor is it a valid ", theParam, "."))
-          }
-          tempOut <- num
-        } else {
-          allSizes <- seq(from = min(theme@parameters[[theParam]], na.rm = TRUE),
-                          to = max(theme@parameters[[theParam]], na.rm = TRUE),
-                          length.out = thebins)
-
-          if(is.null(items)){
-            tempOut <- rep(num, length(allSizes))
-          } else {
-            valCuts <- rank(items, ties.method = "min")
-            tempOut <- allSizes[valCuts]
-          }
-        }
-
-      } else if(theParam %in% c("pointsymbol", "linetype")){
-
-        if(!is.null(theme@scale$bins)){
-          thebins <- theme@scale$bins
-        } else {
-          thebins <- length(items)
-        }
-
-        if(is.null(items)){
-          if(is.na(num)){
-            stop(paste0("'", theVar, "' was neither found as column in the object to plot, nor is it a valid ", theParam, "."))
-          }
-          tempOut <- num
-        } else {
-          allSymbols <- theme@parameters[[theParam]]
-
-          if(is.null(items)){
-            tempOut <- rep(num, length(allSymbols))
-          } else {
-            valCuts <- rank(items, ties.method = "min")
-            tempOut <- allSymbols[valCuts]
-          }
-        }
-
+      # if the parameter to scale has not beend defined as quick parameter, add it to 'tempArgs'
+      if(!theme@scale$param %in% names(plotParams) & !is.na(theme@scale$param)){
+        tempArgs <- c(tempArgs, setNames(list(theme@scale$to), theme@scale$param))
       }
 
-      params[[pos]] <- tempOut
-    }
+      # process parameters that shall be changed
+      for(i in seq_along(tempArgs)){
 
-    # rescale values between 0 and 1
-    x <- gt_scale(obj = x, range = tibble(x = c(0, 1), y = c(0, 1)))
+        # determine value and name of the i-th display argument
+        theVar <- tempArgs[[i]]
+        theParam <- names(tempArgs)[i]
+        pos <- which(names(params) %in% theParam)
 
-    point <- getPoints(x = x)
-    ids <- unique(point$fid)
+        items <- suppressMessages(gt_pull(obj = x, var = theVar))
+        num <- suppressWarnings(as.numeric(as.character(theVar)))
 
-    if(featureType[1] %in% "point"){
+        # if the argument is a colour argument, construct a color ramp from two or more values
+        if(theParam %in% c("linecol", "fillcol")){
 
-      out <- pointsGrob(x = unit(point$x, "npc"),
-                        y = unit(point$y, "npc"),
-                        pch = params$pointsymbol,
+          if(!is.null(theme@scale$bins)){
+            thebins <- theme@scale$bins
+          } else {
+            thebins <- length(items)
+          }
+
+          if(is.null(items)){
+            cols <- theVar
+            if(!any(as.character(cols) %in% colors()) & !any(grepl(pattern = "\\#(.{6,8})", x = cols))){
+              stop(paste0("'", cols, "' was neither found as column in the object to plot, nor is it a valid colour."))
+            }
+            tempOut <- colorRampPalette(colors = cols)(length(theVar))
+          } else {
+            cols <- theme@parameters$colours
+            allColours <- colorRampPalette(colors = cols)(thebins)
+
+            valCuts <- rank(items, ties.method = "min")
+            tempOut <- allColours[valCuts]
+          }
+
+          if(!is.null(theme@parameters$missingcol)){
+            tempOut[is.na(tempOut)] <- theme@parameters$missingcol
+          }
+
+        } else if(theParam %in% c("linewidth", "pointsize")){
+
+          if(!is.null(theme@scale$bins)){
+            thebins <- theme@scale$bins
+          } else {
+            thebins <- length(items)
+          }
+
+          if(is.null(items)){
+            if(is.na(num)){
+              stop(paste0("'", theVar, "' was neither found as column in the object to plot, nor is it a valid ", theParam, "."))
+            }
+            tempOut <- num
+          } else {
+            allSizes <- seq(from = min(theme@parameters[[theParam]], na.rm = TRUE),
+                            to = max(theme@parameters[[theParam]], na.rm = TRUE),
+                            length.out = thebins)
+
+            if(is.null(items)){
+              tempOut <- rep(num, length(allSizes))
+            } else {
+              valCuts <- rank(items, ties.method = "min")
+              tempOut <- allSizes[valCuts]
+            }
+          }
+
+        } else if(theParam %in% c("pointsymbol", "linetype")){
+
+          if(!is.null(theme@scale$bins)){
+            thebins <- theme@scale$bins
+          } else {
+            thebins <- length(items)
+          }
+
+          if(is.null(items)){
+            if(is.na(num)){
+              stop(paste0("'", theVar, "' was neither found as column in the object to plot, nor is it a valid ", theParam, "."))
+            }
+            tempOut <- num
+          } else {
+            allSymbols <- theme@parameters[[theParam]]
+
+            if(is.null(items)){
+              tempOut <- rep(num, length(allSymbols))
+            } else {
+              valCuts <- rank(items, ties.method = "min")
+              tempOut <- allSymbols[valCuts]
+            }
+          }
+
+        }
+
+        params[[pos]] <- tempOut
+      }
+
+      # rescale values between 0 and 1
+      x <- gt_scale(obj = x, range = tibble(x = c(0, 1), y = c(0, 1)))
+
+      point <- getPoints(x = x)
+      ids <- unique(point$fid)
+
+      if(featureType[1] %in% "point"){
+
+        out <- pointsGrob(x = unit(point$x, "npc"),
+                          y = unit(point$y, "npc"),
+                          pch = params$pointsymbol,
+                          name = ids,
+                          size = unit(params$pointsize, "char"),
+                          gp = gpar(
+                            col = params$linecol,
+                            fill = params$fillcol))
+
+      } else if(featureType[1] %in% "line"){
+
+        out <- polylineGrob(x = unit(point$x, "npc"),
+                            y = unit(point$y, "npc"),
+                            id = as.numeric(as.factor(point$fid)),
+                            name = ids,
+                            gp = gpar(col = params$linecol,
+                                      lty = params$linetype,
+                                      lwd = params$linewidth))
+
+      } else if(featureType[1] %in% "polygon"){
+
+        dups <- group_by(.data = point, fid, x, y)
+        dups <- mutate(.data = dups,
+                       is_dup = duplicated(x) & duplicated(y),
+                       is_odd = seq_along(fid) %% 2 == 0,
+                       dup = as.integer(is_dup & is_odd))
+        dups <- dups[["dup"]]
+        dups <- c(0, dups[-length(dups)])
+        vids <- 1 + cumsum(dups)
+
+        out <- pathGrob(x = point$x,
+                        y = point$y,
+                        id = vids,
+                        pathId = point$fid,
+                        rule = "evenodd",
                         name = ids,
-                        size = unit(params$pointsize, "char"),
                         gp = gpar(
                           col = params$linecol,
-                          fill = params$fillcol))
+                          fill = params$fillcol,
+                          lty = params$linetype,
+                          lwd = params$linewidth))
+      }
 
-    } else if(featureType[1] %in% "line"){
+    } else {
 
-      out <- polylineGrob(x = unit(point$x, "npc"),
-                          y = unit(point$y, "npc"),
-                          id = as.numeric(as.factor(point$fid)),
-                          name = ids,
-                          gp = gpar(col = params$linecol,
-                                    lty = params$linetype,
-                                    lwd = params$linewidth))
+      vals <- getFeatures(x = x)$values
 
-    } else if(featureType[1] %in% "polygon"){
+      if(testCharacter(x = vals, pattern = "\\#(.{6,8})")){
+        theColours <- as.vector(vals)
+      } else {
+        items <- sort(gt_pull(obj = x, var = theme@scale$to))
+        breaksTemp <- c(items[1]-1, items)
+        valCuts <- cut(vals, breaks = breaksTemp, include.lowest = TRUE)
 
-      dups <- group_by(.data = point, fid, x, y)
-      dups <- mutate(.data = dups,
-                     is_dup = duplicated(x) & duplicated(y),
-                     is_odd = seq_along(fid) %% 2 == 0,
-                     dup = as.integer(is_dup & is_odd))
-      dups <- dups[["dup"]]
-      dups <- c(0, dups[-length(dups)])
-      vids <- 1 + cumsum(dups)
+        cols <- theme@parameters$colours
+        allColours <- colorRampPalette(colors = cols)(theme@scale$bins)
 
-      out <- pathGrob(x = point$x,
-                      y = point$y,
-                      id = vids,
-                      pathId = point$fid,
-                      rule = "evenodd",
-                      name = ids,
-                      gp = gpar(
-                        col = params$linecol,
-                        fill = params$fillcol,
-                        lty = params$linetype,
-                        lwd = params$linewidth))
+        theColours <- allColours[valCuts]
+      }
+
+      out <- rasterGrob(x = unit(0, "npc"),
+                        y = unit(0, "npc"),
+                        width = unit(1, "npc"),
+                        height = unit(1, "npc"),
+                        hjust = 0,
+                        vjust = 0,
+                        image = matrix(data = theColours, nrow = getRows(x = x), ncol = getCols(x = x), byrow = TRUE),
+                        name = "theRaster",
+                        interpolate = FALSE)
+    }
+
+    if(is(out) != "gList"){
+      out <- gList(out)
     }
 
   } else {
-
-    vals <- getFeatures(x = x)$values
-
-    if(testCharacter(x = vals, pattern = "\\#(.{6,8})")){
-      theColours <- as.vector(vals)
-    } else {
-      items <- sort(gt_pull(obj = x, var = theme@scale$to))
-      breaksTemp <- c(items[1]-1, items)
-      valCuts <- cut(vals, breaks = breaksTemp, include.lowest = TRUE)
-
-      cols <- theme@parameters$colours
-      allColours <- colorRampPalette(colors = cols)(theme@scale$bins)
-
-      theColours <- allColours[valCuts]
-    }
-
-    out <- rasterGrob(x = unit(0, "npc"),
-                      y = unit(0, "npc"),
-                      width = unit(1, "npc"),
-                      height = unit(1, "npc"),
-                      hjust = 0,
-                      vjust = 0,
-                      image = matrix(data = theColours, nrow = getRows(x = x), ncol = getCols(x = x), byrow = TRUE),
-                      name = "theRaster",
-                      interpolate = FALSE)
-  }
-
-  if(is(out) != "gList"){
-    out <- gList(out)
+    out <- NULL
   }
 
   return(out)
