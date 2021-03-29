9a3120a168e71b3c90229feb1e9864f98654973a
Steffen Ehrmann <steffen.ehrmann@idiv.de>
2021-03-29 11:50:34 +0200

    don't allow geoms other than the basic types for now

--- R/gs_random.R
+++ R/gs_random.R
@@ -2,8 +2,9 @@
 #'
 #' This function creates a random geometry
 #' @param type [\code{character(1)}]\cr Either one of the three main feature
-#'   types \code{"point"}, \code{"line"} or \code{"polygon"}, or more
-#'   specifically one of their subtypes, e.g. \code{"hexagon"}.
+#'   types \code{"point"}, \code{"line"} or \code{"polygon"}, \code{"random"},
+#'   or more specifically one of their subtypes, e.g. \code{"hexagon"} (subtypes
+#'   currently not yet supported).
 #' @param window [\code{data.frame(1)}]\cr in case the reference window deviates
 #'   from the bounding box [0, 1] (minimum and maximum values), specify this
 #'   here.
@@ -12,26 +13,27 @@
 #'   vertices already. If left at \code{NULL} the minimum number of vertices for
 #'   the \code{geom} type, i.e. 1 for \code{point}, 2 for \code{line} and 3 for
 #'   \code{polygon}.
-#' @param ... [various]\cr additional arguments.
+#' @return A \code{geom}.
 #' @family geometry shapes
 #' @examples
-#' input <- matrix(nrow = 100, ncol = 100, data = 0)
-#'
 #' # create a random polygon with five vertices
 #' set.seed(1)
 #' someGeom <- gs_random(type = "polygon", vertices = 5)
 #' visualise(geom = someGeom)
 #'
 #' # in case template is given, this serves as source for the window extent
-#' visualise(geom = gs_random(template = input), new = FALSE, linecol = "red")
+#' visualise(geom = gs_random(), new = FALSE, linecol = "red")
 #' @importFrom stats runif
 #' @export
 
-gs_random <- function(type = "point", window = NULL, vertices = NULL, ...){
+gs_random <- function(type = "point", window = NULL, vertices = NULL){
 
-  theCoices <- c("point", "line", "polygon", "triangle", "rectangle", "square", "hexagon", "random")
-  assertSubset(x = type, choices = theCoices)
-  theWindow <- .testWindow(x = window, ...)
+  theCoices <- c("point", "line", "polygon")
+  assertSubset(x = type, choices = c(theCoices, "random"))
+  if(type == "random"){
+    type <- sample(x = theCoices, size = 1)
+  }
+  theWindow <- .testWindow(x = window)
   assertIntegerish(vertices, any.missing = FALSE, len = 1, null.ok = TRUE)
 
   if(type == "point"){
