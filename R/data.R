f3289f8a2aade7dc81b47813477fb45622de0fbe
Steffen Ehrmann <steffen.ehrmann@idiv.de>
2021-11-11 19:01:27 +0100

    exclude objects of other classes

--- R/data.R
+++ R/data.R
@@ -1,39 +1,12 @@
-#' Example \code{RasterStack} object
-#'
-#' A set of two conceptually different types of raster.
-#'
-#' @format The object of class RasterStack has no projection and is a
-#'   RasterStack object of 56 by 60 cells. The first raster represents
-#'   land-use classes and the second raster contains a continuous scale of
-#'   vegetation cover.
-"gtRasters"
-
 #' Example \code{geom} objects
 #'
 #' A set of five geometries.
 #' @format The list contains five objects of class geom, a \code{point},
-#'   \code{line} and \code{polygon} object and two \code{grid} geoms, one with
-#'   categorical data and one with continuous data. They are mostly used in the
+#'   \code{line} and \code{polygon} object and two \code{grid} geoms (that have
+#'   56 by 60 cells), one with categorical values (land-use classes) and one
+#'   with continuous values (vegetation cover). They are mostly used in the
 #'   example and test-sections of this package.
 "gtGeoms"
-
-#' Example \code{Spatial} objects
-#'
-#' A set of four sp objects
-#' @format The list contains four objects of class \code{Spatial}, a
-#'   \code{SpatialPoints}, a \code{SpatialMultiPoints}, a, \code{SpatialLines}
-#'   and a \code{SpatialPolygons} object. They are mostly used in the example
-#'   and test-sections of this package.
-"gtSP"
-
-#' Example \code{sf} objects
-#'
-#' A set of six sp objects
-#' @format The list contains six objects of class \code{sf}, a \code{POINT}, a
-#'   \code{MULTIPOINT}, a \code{LINESTRING}, a \code{MULTILINESTRING}, a
-#'   \code{POLYGON}, and a \code{MULTIPOLYGON} object. They are mostly used in
-#'   the example and test-sections of this package.
-"gtSF"
 
 #' Default visualising theme
 #'
