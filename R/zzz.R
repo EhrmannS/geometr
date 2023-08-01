38ddf63d0239d2ac75ca2538f33d741a545fb24e
EhrmannS <steffen.ehrmann@posteo.de>
2023-08-01 18:22:30 +0200

    update documentation

--- R/zzz.R
+++
@@ -1,3 +0,0 @@
-#' @useDynLib geometr
-globalVariables(c("x", "y", "gtTheme", "fid", "gid", "vid", "targetGrob",
-                  "include", "L1", "L2", "L3", "is_dup", "is_odd", "value"))
--- tests/testthat/test_getLayers.R
+++
@@ -1,2 +0,0 @@
-library(checkmate)
-library(testthat)
